71399c06f64bab06de2295dd60b8cc30c7c07f43
Thiên Kim <thienkimbui10@gmail.com>
2025-05-15 23:53:36 +0700

    Add notification

---
+++ src/main/java/com/api/bee_smart_backend/repository/NotificationRepository.java
@@ -0,0 +1,15 @@
+package com.api.bee_smart_backend.repository;
+
+import com.api.bee_smart_backend.model.Notification;
+import com.api.bee_smart_backend.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+    List<Notification> findByUserOrderByCreatedAtDesc(User user);
+
+    List<Notification> findByUserAndReadFalse(User user);
+
+    List<Notification> findByUserAndDeletedAtIsNullOrderByCreatedAtDesc(User user);
+}
